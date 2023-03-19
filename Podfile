4ae371dfc00d02a69474409f55f08c3b607f4eb0
Mursel Elibol <mursel.elibol@naylalabs.com>
2023-03-19 23:03:32 +0300

    init pod

---
+++ Podfile
@@ -0,0 +1,9 @@
+# platform :ios, '9.0'
+
+target 'MovieTrackerV2' do
+  use_frameworks!
+
+  pod 'Alamofire'
+  pod 'SDWebImage', '~> 5.0'
+
+end
---
+++ Pods/Target Support Files/Pods-MovieTrackerV2/Pods-MovieTrackerV2-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MovieTrackerV2 : NSObject
+@end
+@implementation PodsDummy_Pods_MovieTrackerV2
+@end
